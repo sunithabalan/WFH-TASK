d45a881425ae06a35d21a1ce45fc94f84a976d0c
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-03-23 17:40:23 +0530

    Update BatchAllocation1.swift

--- BatchAllocation1.swift
+++ BatchAllocation1.swift
@@ -1,28 +1,26 @@
 import Foundation
-var ordervalue = 100
-let cases1 = 50
-let outer1 = 5
-var total_available = 0
+var orderValue = 100
+let cases = 50
+let outer = 5
 var batch = [["avail": 50, "name": "B1", "cases":0, "outer": 0, "pieces": 0],
             ["avail": 25, "name": "B2", "cases":0, "outer": 0, "pieces": 0],
             ["avail": 125, "name": "B3", "cases":0, "outer": 0, "pieces": 0],
-           
             ]
 for each in 0..<batch.count
 {
 
-if batch[each]["avail"] as! Int >= cases1, ordervalue >= outer1
+if batch[each]["avail"] as! Int >= cases, orderValue >= outer
 {
-    ordervalue = ordervalue - cases1
+    orderValue = orderValue - cases
     batch[each]["cases"] = batch[each]["cases"] as! Int + 1
-    total_available = total_available - cases1
+    
 }
-else if batch[each]["avail"] as! Int >= outer1, ordervalue >= outer1
+else if batch[each]["avail"] as! Int >= outer, orderValue >= outer
 {
-    ordervalue = ordervalue - cases1
+    orderValue = orderValue - cases
     batch[each]["outer"] = batch[each]["outer"] as! Int + 1
 
 }
 }
 print(batch)
-print(total_available)
+
