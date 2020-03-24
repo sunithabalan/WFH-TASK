59e6799a43ef6672be00f15961f20cf423de4816
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-03-24 17:59:53 +0530

    Update batch3.swift

--- batch3.swift
+++ batch3.swift
@@ -1,43 +1,51 @@
 import Foundation
-
 var orderValue = 100
 let cases = 50
 let outer = 5
-var batch = [["avail": 150, "name": "B1", "cases":0, "outer": 0, "pieces": 0],
+var batch = [["avail": 50, "name": "B1", "cases":0, "outer": 0, "pieces": 0],
             ["avail": 25, "name": "B2", "cases":0, "outer": 0, "pieces": 0],
             ["avail": 125, "name": "B3", "cases":0, "outer": 0, "pieces": 0],
             ]
-var values = 0
-var  values1 = 0
-var balance = 0
-for each in 0..<batch.count
+var  valuesOfCase = 0
+var  valuesOfOrder = 0
+
+for product in 0..<batch.count
 {
 
-if batch[each]["avail"] as! Int >= cases, orderValue >= cases , cases > 0
+if batch[product]["avail"] as! Int >= cases, orderValue >= cases , cases > 0
 {  
-    values = batch[each]["avail"] as! Int / cases
-    values1 = orderValue / cases
-    if values<values1
+   valuesOfCase = batch[product]["avail"] as! Int / cases
+   valuesOfOrder = orderValue / cases
+    if valuesOfCase<valuesOfOrder
     {
-        batch[each]["cases"] = batch[each]["cases"] as! Int + values
-        batch[each]["avail"] = abs(batch[each]["avail"] as! Int - (values * cases))
-        orderValue =  orderValue - (values * cases)
+        batch[product]["cases"] = batch[product]["cases"] as! Int + valuesOfCase
+        batch[product]["avail"] = batch[product]["avail"] as! Int - (valuesOfCase * cases)
+        orderValue =  orderValue - (valuesOfCase * cases)
     }
      else
      {
-         batch[each]["cases"] = batch[each]["cases"] as! Int + values1
-         batch[each]["avail"] = abs(batch[each]["avail"] as! Int - (values1 * cases))
-         orderValue =  orderValue - (values1 * cases)
+         batch[product]["cases"] = batch[product]["cases"] as! Int + valuesOfOrder
+         batch[product]["avail"] = batch[product]["avail"] as! Int - (valuesOfOrder * cases)
+         orderValue =  orderValue - (valuesOfOrder * cases)
   
      }
 }
-else if batch[each]["avail"] as! Int >= outer, orderValue >= outer
+else if batch[product]["avail"] as! Int >= outer, orderValue >= outer, outer > 0
  {
-     values = batch[each]["avail"] as! Int / outer
-     values1 = orderValue / outer
-     batch[each]["outer"] = batch[each]["outer"] as! Int + values1
-     batch[each]["avail"] = abs( batch[each]["avail"] as! Int - (values1 * outer))
-}
-
+   valuesOfCase = batch[product]["avail"] as! Int / outer
+   valuesOfOrder = orderValue / outer
+    if valuesOfCase<valuesOfOrder
+    {
+     batch[product]["outer"] = batch[product]["outer"] as! Int + valuesOfCase
+     batch[product]["avail"] = batch[product]["avail"] as! Int - (valuesOfCase * outer)
+     orderValue =  orderValue - (valuesOfCase * outer)
+    }
+    else
+    {
+    batch[product]["outer"] = batch[product]["outer"] as! Int + valuesOfOrder
+    batch[product]["avail"] = abs( batch[product]["avail"] as! Int - (valuesOfOrder * outer) )
+    orderValue =  orderValue - (valuesOfOrder * outer)
+    }
  }
+}
  print(batch)
