b00d05f09e2a003a9bba28a6dff0922946a4fb34
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-04-10 15:27:38 +0530

    Create ElectricitybillCalculation.swift

---
+++ ElectricitybillCalculation.swift
@@ -0,0 +1,73 @@
+// Need to calculate Electricity bill for every two months based on below tariffs.  
+// For domestic
+// For first 100 units Rs 0/unit
+// For next 100 units Rs 3.50/unit
+// For next 300 units Rs 4.60/unit
+// For unit above 500  Rs 6.60/unit
+// Fixed charges for two months Rs.50/service
+
+// For Commercial
+// For first 100 units Rs 5.00/unit
+// Consumption above 100 units bi monthly 8.05/unit
+// Fixed charges for two months Rs.290/service
+
+// Get random numbers as daily consumable units. For domestic random number generation should be in between 1 - 10. For commercial 10 - 100
+
+import Foundation
+let bill = "commercial"
+var amount:Double = 0
+var total = 0
+if bill == "Domestic"
+{
+let perDayUnit = Int.random(in:1...10)
+print("The one day unit is",perDayUnit)
+let units  = perDayUnit * 60
+print("Two month unit", units)
+let surchargeForDomestic:Double = 50
+if(units <= 100)
+{
+    amount = Double(units * 0)
+    total = Int(amount + surchargeForDomestic)
+    print("The Electricity bill is",total)
+}
+else if(units > 101 && units <= 200 )
+{
+    amount = (Double(units - 100) * 3.50)
+    total = Int(amount + surchargeForDomestic)
+    print("The Electricity bill is",total)
+}
+else if(units >= 201 && units <= 500)
+{
+    amount =  (Double(units - 100) * 3.50) + (Double(units - 200) * 4.60)
+    total = Int(amount + surchargeForDomestic)
+    print("The Electricity bill is",total)
+  
+}
+else
+{
+    amount = Double(units) * 6.60
+    total = Int(amount + surchargeForDomestic)
+    print("The Electricity bill is",total)
+}
+}
+else 
+{
+  let perDayUnit = Int.random(in:10...100)
+  print("The one day unit is",perDayUnit)
+  let units  = perDayUnit * 60
+  let surchargeForCommercial:Double  = 290
+  print("Two month unit", units)
+if(units >= 100)
+{
+    amount = Double(units) * 5.00
+    total = Int(amount + surchargeForCommercial)
+    print("The Electricity bill for Commerial is",total)
+}
+else
+{
+    amount = Double(units) * 8.05
+    total = Int(amount + surchargeForCommercial)
+    print("The Electricity bill for Commerial is",total)
+
+}
+}
