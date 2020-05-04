18b502f4884d353cc7245ef2c387f18979449c1f
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-05-04 13:42:20 +0530

    Update Kruskal.swift

--- Kruskal.swift
+++ Kruskal.swift
@@ -7,3 +7,21 @@ import Foundation
 
  }
 let sortedWeights = weights.map({ $0 < $1})
+
+import Foundation
+ var verticesEdgesWeight = ["1:2": 8,"2:3": 1,"2:6": 3,"3:6":7,"4:3": 4,"1:4": 6]
+ var vertices = "1:2:3:4:6"
+ var  weights = [String]()
+ for (keys,values)  in verticesEdgesWeight.sorted(by:{$0.1 < $1.1})
+ {
+   var originalkey:String = keys
+   if vertices == originalkey{
+       print(originalkey, values)
+   }
+   else{
+       originalkey.append(keys)
+       
+       }
+       print(originalkey)
+ }
+
