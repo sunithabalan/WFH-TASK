56788502c756c0a2b4aadc5b17f6a6d2f39d56f5
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-05-12 14:36:11 +0530

    Update knapsack.swift

--- knapsack.swift
+++ knapsack.swift
@@ -1,32 +1,28 @@
 import Foundation
 import Glibc
-import Foundation
-func knapsack(w:Int, weight:[Int], value:[Int], number:Int) 
+ 
+func knapsack(w:Int, weight:[Int], value:[Int], number:Int) -> Int
 {
-  var k =  Array(repeating: Array(repeating: 1,count: w+1), count: number+1)
-  for i in 0..<number
+  var k =  Array(repeating: Array(repeating: 0,count: w+1), count: number+1)
+ 
+  for i in 0..<number+1
   {
-    for j in 0..<w
+    for j in 0..<w+1
     {
-      if (i == 0) || (j == 0){
+      if (i == 0) || (j == 0) {
         k[i][j] = 0
-      }
-      else if (weight[i - 1] <= j){
+      } else if (weight[i - 1] <= j) {
         k[i][j] = max(value[i - 1] + k[i - 1][j - weight[i - 1]], k[i - 1][j])
-        print(  k[i][j])
-      }
-      else{
+      } else {
         k[i][j] = k[i - 1][j]
       }
     }
-   
   }
- print(k[number][w])  
-  
-  
+return k[number][w]
 }
 let totalweight = 50
+
 let weights = [10, 20, 30] 
 let values = [60, 100, 120]
-let number = 3
-print(knapsack(w: totalweight, weight: weights, value: values, number:number)) 
+let number = values.count
+print(knapsack(w: totalweight, weight: weights,value: values, number:number))
