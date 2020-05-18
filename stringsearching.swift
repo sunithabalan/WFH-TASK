3537545984c651a628b447493375a0c862b27da9
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-05-18 12:41:33 +0530

    Update stringsearching.swift

--- stringsearching.swift
+++ stringsearching.swift
@@ -24,3 +24,36 @@ func search(pattern:String, text:String)
 let  text = "AABAACAADAABAAABAA"
 let  pattern = "AABA"
 search(pattern:pattern, text:text) 
+
+import Foundation
+func search(pattern:String, text:String)
+{
+    let m = pattern.count
+    let n = text.count
+    var pat = [pattern]
+    var txt = [text]
+    var i = 0
+     
+    while(i <= n-m){
+      var j = 0
+      for j in 0..<m{
+        if txt[i+j] != pat[j]{
+          break
+        }
+        j = j + 1
+      }
+      if j == m {
+         print ("Pattern found at index ",i) 
+         i = i + m
+      }
+      else if j == 0 {
+        i = i + 1
+      }
+      else {
+        i = i + j 
+      }
+    }
+}
+let  text = "AABAACAADAABAAABAA"
+let  pattern = "AABA"
+search(pattern:pattern, text:text) 
