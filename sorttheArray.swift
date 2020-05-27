db515e7e2f8a6541b922db6edf8f4156ff3a91f7
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-05-27 12:30:57 +0530

    Update sorttheArray.swift

--- sorttheArray.swift
+++ sorttheArray.swift
@@ -1,23 +1,45 @@
+/* To sort the array using any one of the algorithm ,
+  that uses minimium number of swaps and also find the summation values by 
+  subtracting from one element to other element till the index given.
+
+
+input: Arrayofitems, indexvalue
+output:Sorted array, swap count, Subtracted value
+*/
+
+
+
 import Foundation
+
 let arrayToSort = [9, 22, 50, 10, 33, 41, 21, 40, 80, 60, 26]
-func performSelectionSort(tobeSorted: [Int]) {
-   var result = tobeSorted
-   var count = 0
-   for eachnumber in 0..<result.count{
+let indexvalue  = 3
+func performSelectionSort(tobeSorted: [Int],index:Int) {
+   var arrayOfSortedelements = tobeSorted
+   var numberOfSwap = 0
+   for eachnumber in 0..<arrayOfSortedelements.count{
       var minimiumindex = eachnumber
-      for eachvalues in eachnumber+1..<result.count{
-         if result[eachvalues] < result[minimiumindex]{
-           minimiumindex = eachvalues
-        }
-        }        
-       if minimiumindex != eachnumber{
-          result.swapAt(minimiumindex,eachnumber)
-          count = count + 1
+          for eachvalues in eachnumber+1..<arrayOfSortedelements.count{
+                       if arrayOfSortedelements[eachvalues] < arrayOfSortedelements[minimiumindex]{
+                                 minimiumindex = eachvalues
+                        }
+             }        
+           if minimiumindex != eachnumber{
+                 arrayOfSortedelements.swapAt(minimiumindex,eachnumber)
+                 numberOfSwap = numberOfSwap + 1
           }
     }
-     print(result)
-     print(count)
+     print("The Result of sorted Array",arrayOfSortedelements)
+     print("The number of swap taken is",numberOfSwap)
+     
+    var  sumOfSubtractedValues = 0
+    var  eachiteration = 0
+    while(eachiteration != index){
+          sumOfSubtractedValues += arrayOfSortedelements[eachiteration] - arrayOfSortedelements[eachiteration + 1]
+          eachiteration = eachiteration + 1
+    }
+    print("The summation value is",abs(sumOfSubtractedValues))
      
 }
+performSelectionSort(tobeSorted: arrayToSort,index:indexvalue)
 
-performSelectionSort(tobeSorted: arrayToSort)
+v
