5729d9c76810d8e27c88188bd1632f25bb686ecb
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-05-27 15:39:24 +0530

    Update sorttheArray.swift

--- sorttheArray.swift
+++ sorttheArray.swift
@@ -7,33 +7,32 @@ input: Arrayofitems, indexvalue
 output:Sorted array, swap count, Subtracted value
 */
 
-
 import Foundation
 
-let arrayToSort = [9, 22, 50, 10, 33, 41, 21, 40, 80, 60, 26]
+let arrayToSort = [7,5,4,2,1]
 let indexvalue  = 3
 func performSelectionSort(tobeSorted: [Int],index:Int) {
-   var arrayOfSortedelements = tobeSorted
+   var arrayOfSortingelements = tobeSorted
    var numberOfSwap = 0
-   for eachnumber in 0..<arrayOfSortedelements.count{
+   for eachnumber in 0..<arrayOfSortingelements.count{
       var minimiumindex = eachnumber
-          for eachvalues in eachnumber+1..<arrayOfSortedelements.count{
-                       if arrayOfSortedelements[eachvalues] < arrayOfSortedelements[minimiumindex]{
+          for eachvalues in eachnumber+1..<arrayOfSortingelements.count{
+                       if arrayOfSortingelements[eachvalues] < arrayOfSortingelements[minimiumindex]{
                                  minimiumindex = eachvalues
                         }
              }        
             if minimiumindex != eachnumber{
-                 arrayOfSortedelements.swapAt(minimiumindex,eachnumber)
+                 arrayOfSortingelements.swapAt(minimiumindex,eachnumber)
                  numberOfSwap = numberOfSwap + 1
           }
     }
-    print("The Result of sorted Array",arrayOfSortedelements)
+    print("The Result of sorted Array",arrayOfSortingelements)
     print("The number of swap taken is",numberOfSwap)
      //Finding the minimium value after sorting
     var  sumOfSubtractedValues = 0
     var  eachiteration = 0
             while(eachiteration != index){
-                     sumOfSubtractedValues += arrayOfSortedelements[eachiteration] - arrayOfSortedelements[eachiteration + 1]
+                     sumOfSubtractedValues += arrayOfSortingelements[eachiteration] - arrayOfSortingelements[eachiteration + 1]
                      eachiteration = eachiteration + 1
              }
             print("The summation value is",abs(sumOfSubtractedValues))
