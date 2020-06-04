250254e5f711b48044766e4ed05c7951e024e0d5
sunithabalan <sunithabalasubramanian14@gmail.com>
2020-06-04 16:18:27 +0530

    Update Lcmtask.swift

--- Lcmtask.swift
+++ Lcmtask.swift
@@ -4,20 +4,20 @@ func greastestCommonDivisor(between numberOne:Int, and numberTwo:Int) -> Int
     var largestValue = max(numberOne,numberTwo)
     var smallestValue  = min(numberOne,numberTwo)
     while (smallestValue != 0) {
-       temp = largestValue 
+       temp = largestValue
        largestValue  = smallestValue
        smallestValue =  temp % largestValue
 }
-var gcd = largestValue
+    let gcd = largestValue
 return gcd
 }
 func leastCommonMultiple(between numberOne:Int, and numberTwo:Int, greatestnumber:Int) -> Int
 {
-var lcm = (numberOne * numberTwo) / greatestnumber
+    let lcm = (numberOne * numberTwo) / greatestnumber
 return lcm
 }
 let numberOne = 28
 let numberTwo = 36
-let gcd = greastestCommonDivisor(between :numberOne, and: numberTwo) 
+let gcd = greastestCommonDivisor(between :numberOne, and: numberTwo)
 let lcm = leastCommonMultiple(between : numberOne, and: numberTwo, greatestnumber:gcd)
 print("The Lcm for two number is",lcm)
